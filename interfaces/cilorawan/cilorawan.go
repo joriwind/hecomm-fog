77f339dee36179ea2fb2ea13927ce2b1951757f2
joriwind <j.winderickx@gmail.com>
2017-04-26 14:42:06 +0200

    lorawan interface client

--- interfaces/cilorawan/cilorawan.go
+++ interfaces/cilorawan/cilorawan.go
@@ -6,6 +6,7 @@ import (
 	"net"
 
 	pb "github.com/joriwind/hecomm-fog/api/as"
+	ns "github.com/joriwind/hecomm-fog/api/ns"
 	"github.com/joriwind/hecomm-fog/interfaces"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -37,4 +38,24 @@ func StartServer(ctx context.Context, comLink chan interfaces.ComLinkMessage) er
 	}
 
 	return err
+}
+
+//ConnectToNetworkServer Create connection with LoRaWAN Network server
+func ConnectToNetworkServer(host string, nsDialOptions []grpc.DialOption) (*grpc.ClientConn, ns.NetworkServerClient) {
+	//Does the fog use secured connection?
+	//var asDialOptions []grpc.DialOption
+	/*if c.String("as-tls-cert") != "" && c.String("as-tls-key") != "" {
+		asDialOptions = append(asDialOptions, grpc.WithTransportCredentials(
+			mustGetTransportCredentials(c.String("as-tls-cert"), c.String("as-tls-key"), c.String("as-ca-cert"), false),
+		))
+	} else {*/
+	nsDialOptions = append(nsDialOptions, grpc.WithInsecure())
+	//}
+	//host := "192.168.1.1:8000"
+	nsConn, err := grpc.Dial(host, nsDialOptions...) //TODO: when close connection?
+	if err != nil {
+		log.Fatalf("application-server (FOG) dial error: %s", err)
+	}
+	//defer asConn.Close() //TODO: Do not forget to close connection!
+	return nsConn, ns.NewNetworkServerClient(nsConn)
 }
