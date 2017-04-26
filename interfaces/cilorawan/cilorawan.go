5cf692e235bf551e26f5acf259453ef1bab84aa5
joriwind <j.winderickx@gmail.com>
2017-04-26 15:03:22 +0200

    Cleanup lorawan interface client

--- interfaces/cilorawan/cilorawan.go
+++ interfaces/cilorawan/cilorawan.go
@@ -1,28 +1,3 @@
 package cilorawan
 
-import (
-	"log"
 
-	ns "github.com/joriwind/hecomm-fog/api/ns"
-	"google.golang.org/grpc"
-)
-
-//ConnectToNetworkServer Create connection with LoRaWAN Network server
-func ConnectToNetworkServer(host string, nsDialOptions []grpc.DialOption) (*grpc.ClientConn, ns.NetworkServerClient) {
-	//Does the fog use secured connection?
-	//var asDialOptions []grpc.DialOption
-	/*if c.String("as-tls-cert") != "" && c.String("as-tls-key") != "" {
-		asDialOptions = append(asDialOptions, grpc.WithTransportCredentials(
-			mustGetTransportCredentials(c.String("as-tls-cert"), c.String("as-tls-key"), c.String("as-ca-cert"), false),
-		))
-	} else {*/
-	nsDialOptions = append(nsDialOptions, grpc.WithInsecure())
-	//}
-	//host := "192.168.1.1:8000"
-	nsConn, err := grpc.Dial(host, nsDialOptions...) //TODO: when close connection?
-	if err != nil {
-		log.Fatalf("application-server (FOG) dial error: %s", err)
-	}
-	//defer asConn.Close() //TODO: Do not forget to close connection!
-	return nsConn, ns.NewNetworkServerClient(nsConn)
-}
--- interfaces/cilorawan/client.go
+++ interfaces/cilorawan/client.go
@@ -1,5 +1,43 @@
 package cilorawan
 
-func sendPacket() {
+import (
+	"log"
 
+	ns "github.com/joriwind/hecomm-fog/api/ns"
+	"google.golang.org/grpc"
+)
+
+//NetworkClient Object for interfacing LoRaWAN Network server client
+type NetworkClient struct {
+	Host          string
+	NsDialOptions []grpc.DialOption
+	NsConn        *grpc.ClientConn
+	NetworkClient ns.NetworkServerClient
+}
+
+//NewNetworkClient Create connection with LoRaWAN Network server
+func (n *NetworkClient) NewNetworkClient(host string, nsDialOptions []grpc.DialOption) (*NetworkClient, error) {
+	//Does the fog use secured connection?
+	//var asDialOptions []grpc.DialOption
+	/*if c.String("as-tls-cert") != "" && c.String("as-tls-key") != "" {
+		asDialOptions = append(asDialOptions, grpc.WithTransportCredentials(
+			mustGetTransportCredentials(c.String("as-tls-cert"), c.String("as-tls-key"), c.String("as-ca-cert"), false),
+		))
+	} else {*/
+	nsDialOptions = append(nsDialOptions, grpc.WithInsecure())
+	//}
+	//host := "192.168.1.1:8000"
+	nsConn, err := grpc.Dial(host, nsDialOptions...) //TODO: when close connection?
+	if err != nil {
+		log.Fatalf("application-server (FOG) dial error: %s", err)
+		return &NetworkClient{}, err
+	}
+	//defer asConn.Close() //TODO: Do not forget to close connection!
+	networkClient := ns.NewNetworkServerClient(nsConn)
+	return &NetworkClient{
+		Host:          host,
+		NsDialOptions: nsDialOptions,
+		NsConn:        nsConn,
+		NetworkClient: networkClient,
+	}, nil
 }
