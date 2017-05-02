afb54cebc654b03adaebc8ef808b928694d464fd
joriwind <j.winderickx@gmail.com>
2017-05-02 13:18:26 +0200

    add Delete functions and structure code

--- dbconnection/dbconnection.go
+++ dbconnection/dbconnection.go
@@ -4,6 +4,8 @@ import "database/sql"
 import _ "github.com/go-sql-driver/mysql" //Mysql database
 import "errors"
 import "encoding/json"
+import "fmt"
+import "log"
 
 //Platform Model of a platform in the mysql database
 type Platform struct {
@@ -32,7 +34,7 @@ type Link struct {
 }
 
 const (
-	dbsource string = "hecomm:password@tcp(localhost:5555)/dbname?charset=utf8"
+	dbsource string = "hecomm:hecomm@tcp(localhost:3306)/hecomm?charset=utf8"
 	dbDriver string = "mysql"
 )
 
@@ -45,7 +47,7 @@ func InsertPlatform(pl *Platform) error {
 	defer db.Close()
 
 	//Prepare insert query
-	stmt, err := db.Prepare("INSERT platform SET address=?, citype=?, ciargs=?")
+	stmt, err := db.Prepare("INSERT platform SET address=?, citype=?, ciargs=?, tlscert=?, tlskey=?")
 	if err != nil {
 		return err
 	}
@@ -58,7 +60,7 @@ func InsertPlatform(pl *Platform) error {
 	}
 
 	//Execute insert
-	res, err := stmt.Exec(pl.Address, pl.CIType, string(jsonBytes))
+	res, err := stmt.Exec(pl.Address, pl.CIType, string(jsonBytes), pl.TLSCert, pl.TLSKey)
 	if err != nil {
 		return err
 	}
@@ -69,6 +71,7 @@ func InsertPlatform(pl *Platform) error {
 		return err
 	}
 	pl.ID = int(i)
+	log.Printf("Inserted platform: Address: %v, ciargs: %v, citype: %v, id: %v, tlscert: %v, tlskey: %v", pl.Address, pl.CIArgs, pl.CIType, pl.ID, pl.TLSCert, pl.TLSKey)
 	return nil
 
 }
@@ -98,6 +101,75 @@ func UpdatePlatform(pl *Platform) error {
 	}
 	if i, _ := res.RowsAffected(); i != 1 {
 		return errors.New("dbconnection: failed to update platform")
+	}
+	return nil
+}
+
+//GetPlatform Retrieve platform via platform id
+func GetPlatform(id int) *Platform {
+	db, err := sql.Open(dbDriver, dbsource)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	stmt, err := db.Prepare("SELECT * FROM platform WHERE id=?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(id)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+
+		var citype int
+		var address, tlscert, tlskey, ciargs string
+		if err := rows.Scan(&id, &address, &tlscert, &tlskey, &citype, &ciargs); err != nil {
+			panic(err)
+		}
+		var data map[string]interface{}
+		if err := json.Unmarshal([]byte(ciargs), &data); err != nil {
+			fmt.Printf("Platform from query: %v, %v, %v, %v, %v, %v\n", id, citype, address, tlscert, tlskey, ciargs)
+			panic(err)
+		}
+		return &Platform{
+			ID:      id,
+			Address: address,
+			TLSCert: tlscert,
+			TLSKey:  tlskey,
+			CIType:  citype,
+			CIArgs:  data,
+		}
+	}
+	return &Platform{}
+}
+
+//DeletePlatform Delete platform via platform id
+func DeletePlatform(id int) error {
+	db, err := sql.Open(dbDriver, dbsource)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	stmt, err := db.Prepare("DELETE FROM platform WHERE id=?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(id)
+
+	if err != nil {
+		return err
+	}
+	if i, err := res.RowsAffected(); i != 1 {
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("DeletePlatform, Delete result did not equal 1:%v", i)
 	}
 	return nil
 }
@@ -148,6 +220,60 @@ func UpdateNode(n *Node) error {
 		return errors.New("dbconnection: failed to insert node")
 	}
 	return nil
+}
+
+//DeleteNode Delete node via id
+func DeleteNode(id int) error {
+	db, err := sql.Open(dbDriver, dbsource)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	stmt, err := db.Prepare("DELETE FROM node WHERE id=?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(id)
+
+	if err != nil {
+		return err
+	}
+	if i, err := res.RowsAffected(); i != 1 {
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("DeleteNode, Delete result did not equal 1:%v", i)
+	}
+	return nil
+}
+
+//GetNode Retrieve node via device identifier
+func GetNode(devID []byte) *Node {
+	db, err := sql.Open(dbDriver, dbsource)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	stmt, err := db.Prepare("SELECT * FROM node WHERE devid=?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRow(devID)
+	var id, platformid, inftype int
+	var devid []byte
+	var isprovider bool
+	row.Scan(&id, &devid, &platformid, &isprovider, &inftype)
+	return &Node{
+		ID:         id,
+		DevID:      devid,
+		PlatformID: platformid,
+		IsProvider: isprovider,
+		InfType:    inftype,
+	}
 }
 
 //InsertLink Insert a link into the database
@@ -221,60 +347,29 @@ func GetLink(nodeID int) *Link {
 	}
 }
 
-//GetNode Retrieve node via device identifier
-func GetNode(devID []byte) *Node {
+//DeleteLink Delete link via id
+func DeleteLink(id int) error {
 	db, err := sql.Open(dbDriver, dbsource)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer db.Close()
-	stmt, err := db.Prepare("SELECT * FROM node WHERE devid=?")
+	stmt, err := db.Prepare("DELETE FROM link WHERE id=?")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer stmt.Close()
 
-	row := stmt.QueryRow(devID)
-	var id, platformid, inftype int
-	var devid []byte
-	var isprovider bool
-	row.Scan(&id, &devid, &platformid, &isprovider, &inftype)
-	return &Node{
-		ID:         id,
-		DevID:      devid,
-		PlatformID: platformid,
-		IsProvider: isprovider,
-		InfType:    inftype,
-	}
-}
+	res, err := stmt.Exec(id)
 
-//GetPlatform Retrieve platform via platform id
-func GetPlatform(id int) *Platform {
-	db, err := sql.Open(dbDriver, dbsource)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	defer db.Close()
-	stmt, err := db.Prepare("SELECT * FROM platform WHERE id=?")
-	if err != nil {
-		panic(err)
+	if i, err := res.RowsAffected(); i != 1 {
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("DeleteLink, Delete result did not equal 1:%v", i)
 	}
-	defer stmt.Close()
-
-	row := stmt.QueryRow(id)
-	var citype int
-	var address, tlscert, tlskey, ciargs string
-	row.Scan(&id, &citype, &address, &tlscert, &tlskey, &ciargs)
-	var data map[string]interface{}
-	if err := json.Unmarshal([]byte(ciargs), &data); err != nil {
-		panic(err)
-	}
-	return &Platform{
-		ID:      id,
-		Address: address,
-		TLSCert: tlscert,
-		TLSKey:  tlskey,
-		CIType:  citype,
-		CIArgs:  data,
-	}
+	return nil
 }
