7ecf7b50a550ea0bfc7bebb908e1b76e46624c29
joriwind <j.winderickx@gmail.com>
2017-05-03 13:11:47 +0200

    Add find node

--- dbconnection/dbconnection.go
+++ dbconnection/dbconnection.go
@@ -1,11 +1,17 @@
 package dbconnection
 
-import "database/sql"
-import _ "github.com/go-sql-driver/mysql" //Mysql database
-import "errors"
-import "encoding/json"
-import "fmt"
-import "log"
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joriwind/hecomm-fog/iotInterface"
+)
+
+//Mysql database
 
 //Platform Model of a platform in the mysql database
 type Platform struct {
@@ -106,21 +112,23 @@ func UpdatePlatform(pl *Platform) error {
 }
 
 //GetPlatform Retrieve platform via platform id
-func GetPlatform(id int) *Platform {
+func GetPlatform(id int) (*Platform, error) {
+	var platform Platform
+
 	db, err := sql.Open(dbDriver, dbsource)
 	if err != nil {
-		panic(err)
+		return &platform, err
 	}
 	defer db.Close()
 	stmt, err := db.Prepare("SELECT * FROM platform WHERE id=?")
 	if err != nil {
-		panic(err)
+		return &platform, err
 	}
 	defer stmt.Close()
 
 	rows, err := stmt.Query(id)
 	if err != nil {
-		panic(err)
+		return &platform, err
 	}
 	defer rows.Close()
 	for rows.Next() {
@@ -128,14 +136,14 @@ func GetPlatform(id int) *Platform {
 		var citype int
 		var address, tlscert, tlskey, ciargs string
 		if err := rows.Scan(&id, &address, &tlscert, &tlskey, &citype, &ciargs); err != nil {
-			panic(err)
+			return &platform, err
 		}
 		var data map[string]interface{}
 		if err := json.Unmarshal([]byte(ciargs), &data); err != nil {
 			fmt.Printf("Platform from query: %v, %v, %v, %v, %v, %v\n", id, citype, address, tlscert, tlskey, ciargs)
-			panic(err)
+			return &platform, err
 		}
-		return &Platform{
+		platform = Platform{
 			ID:      id,
 			Address: address,
 			TLSCert: tlscert,
@@ -143,41 +151,42 @@ func GetPlatform(id int) *Platform {
 			CIType:  citype,
 			CIArgs:  data,
 		}
+		return &platform, nil
 	}
-	return &Platform{}
+	return &platform, err
 }
 
 //GetPlatforms Retrieve all platforms
-func GetPlatforms() []*Platform {
+func GetPlatforms() ([]*Platform, error) {
+	var platforms []*Platform
 	db, err := sql.Open(dbDriver, dbsource)
 	if err != nil {
-		panic(err)
+		return platforms, err
 	}
 	defer db.Close()
 	stmt, err := db.Prepare("SELECT * FROM platform")
 	if err != nil {
-		panic(err)
+		return platforms, err
 	}
 	defer stmt.Close()
 
 	rows, err := stmt.Query()
 	if err != nil {
-		panic(err)
+		return platforms, err
 	}
 	defer rows.Close()
-	var platforms []*Platform
 
 	for rows.Next() {
 
 		var id, citype int
 		var address, tlscert, tlskey, ciargs string
 		if err := rows.Scan(&id, &address, &tlscert, &tlskey, &citype, &ciargs); err != nil {
-			panic(err)
+			return platforms, err
 		}
 		var data map[string]interface{}
 		if err := json.Unmarshal([]byte(ciargs), &data); err != nil {
 			fmt.Printf("Platform from query: %v, %v, %v, %v, %v, %v\n", id, citype, address, tlscert, tlskey, ciargs)
-			panic(err)
+			return platforms, err
 		}
 		platforms = append(platforms, &Platform{
 			ID:      id,
@@ -188,7 +197,7 @@ func GetPlatforms() []*Platform {
 			CIArgs:  data,
 		})
 	}
-	return platforms
+	return platforms, nil
 }
 
 //DeletePlatform Delete platform via platform id
@@ -293,16 +302,17 @@ func DeleteNode(id int) error {
 	return nil
 }
 
-//GetNode Retrieve node via device identifier
-func GetNode(devID []byte) *Node {
+//FindNode Retrieve node via device identifier
+func FindNode(devID []byte) (*Node, error) {
+	var node Node
 	db, err := sql.Open(dbDriver, dbsource)
 	if err != nil {
-		panic(err)
+		return &node, err
 	}
 	defer db.Close()
 	stmt, err := db.Prepare("SELECT * FROM node WHERE devid=?")
 	if err != nil {
-		panic(err)
+		return &node, err
 	}
 	defer stmt.Close()
 
@@ -311,13 +321,43 @@ func GetNode(devID []byte) *Node {
 	var devid []byte
 	var isprovider bool
 	row.Scan(&id, &devid, &platformid, &isprovider, &inftype)
-	return &Node{
+	node = Node{
 		ID:         id,
 		DevID:      devid,
 		PlatformID: platformid,
 		IsProvider: isprovider,
 		InfType:    inftype,
 	}
+	return &node, err
+}
+
+//GetNode Retrieve node via device identifier
+func GetNode(ID int) (*Node, error) {
+	var node Node
+	db, err := sql.Open(dbDriver, dbsource)
+	if err != nil {
+		return &node, err
+	}
+	defer db.Close()
+	stmt, err := db.Prepare("SELECT * FROM node WHERE id=?")
+	if err != nil {
+		return &node, err
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRow(ID)
+	var id, platformid, inftype int
+	var devid []byte
+	var isprovider bool
+	row.Scan(&id, &devid, &platformid, &isprovider, &inftype)
+	node = Node{
+		ID:         id,
+		DevID:      devid,
+		PlatformID: platformid,
+		IsProvider: isprovider,
+		InfType:    inftype,
+	}
+	return &node, err
 }
 
 //InsertLink Insert a link into the database
@@ -369,26 +409,28 @@ func UpdateLink(l *Link) error {
 }
 
 //GetLink Retrieve via one of both's node ID
-func GetLink(nodeID int) *Link {
+func GetLink(nodeID int) (*Link, error) {
+	var link Link
 	db, err := sql.Open(dbDriver, dbsource)
 	if err != nil {
-		panic(err)
+		return &link, err
 	}
 	defer db.Close()
 	stmt, err := db.Prepare("SELECT id, provnode, reqnode FROM link WHERE provnode=? OR reqnode=?")
 	if err != nil {
-		panic(err)
+		return &link, err
 	}
 	defer stmt.Close()
 
 	row := stmt.QueryRow(nodeID, nodeID)
 	var id, provnode, reqnode int
 	row.Scan(&id, &provnode, &reqnode)
-	return &Link{
+	link = Link{
 		ID:       id,
 		ProvNode: provnode,
 		ReqNode:  reqnode,
 	}
+	return &link, err
 }
 
 //DeleteLink Delete link via id
@@ -416,4 +458,32 @@ func DeleteLink(id int) error {
 		return fmt.Errorf("DeleteLink, Delete result did not equal 1:%v", i)
 	}
 	return nil
+}
+
+//GetDestination fill message with destination and return the destination node
+func GetDestination(message *iotInterface.ComLinkMessage) (*Node, error) {
+
+	srcnode, err := FindNode(message.Origin)
+	if err != nil {
+		return nil, err
+	}
+
+	link, err := GetLink(srcnode.ID)
+	if err != nil {
+		return nil, err
+	}
+	var dstnode *Node
+	switch srcnode.ID {
+	case link.ProvNode:
+		dstnode, err = GetNode(link.ReqNode)
+	case link.ReqNode:
+		dstnode, err = GetNode(link.ProvNode)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	message.Destination = dstnode.DevID
+
+	return dstnode, nil
 }
