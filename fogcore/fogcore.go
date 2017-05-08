3018a3cb5b90f7cd6186fec6d0a45f30cc49f282
joriwind <j.winderickx@gmail.com>
2017-05-08 11:32:23 +0200

    tls interface

--- fogcore/fogcore.go
+++ fogcore/fogcore.go
@@ -9,7 +9,12 @@ import (
 	"log"
 	"net"
 
+	"time"
+
+	"encoding/json"
+
 	"github.com/joriwind/hecomm-fog/dbconnection"
+	"github.com/joriwind/hecomm-fog/hecomm"
 	"github.com/joriwind/hecomm-fog/iotInterface"
 	"github.com/joriwind/hecomm-fog/iotInterface/cilorawan"
 	"github.com/joriwind/hecomm-fog/iotInterface/cisixlowpan"
@@ -18,11 +23,11 @@ import (
 
 //Fogcore Struct
 type Fogcore struct {
-	ctx             context.Context
-	opt             Options
-	controlChannel  chan controlMessage
-	ciCommonChannel chan iotInterface.ComLinkMessage
-	ciCollection    []*ci
+	ctx          context.Context
+	opt          Options
+	controlCH    chan controlCHMessage
+	ciCommonCH   chan iotInterface.ComLinkMessage
+	ciCollection []*ci
 }
 
 //Options Defines possible options to pass along with Fogcore object
@@ -33,15 +38,24 @@ type Options struct {
 }
 
 type ci struct {
-	platform *dbconnection.Platform
-	channel  chan iotInterface.ComLinkMessage
-	ctx      context.Context
-	cancel   func()
+	Platform *dbconnection.Platform
+	Channel  chan iotInterface.ComLinkMessage
+	Ctx      context.Context
+	Cancel   func()
 }
 
-type controlMessage struct {
-	command  string
-	platform *dbconnection.Platform
+type controlCHMessage struct {
+	Message    hecomm.DBCommand
+	ResponseCH chan bool
+}
+
+type linkState struct {
+	State    int
+	ReqConn  net.Conn
+	ProvConn net.Conn
+	BufReq   []byte
+	BufProv  []byte
+	Ctx      context.Context
 }
 
 //NewFogcore Create new fogcore module
@@ -63,6 +77,7 @@ func NewFogcore(ctx context.Context, opt Options) *Fogcore {
 //Start Start the fogcore module
 func (f *Fogcore) Start() error {
 	//Start management interface
+	f.controlCH = make(chan controlCHMessage, 20)
 	go f.listenOnTLS()
 
 	//Startup already known platforms
@@ -72,24 +87,26 @@ func (f *Fogcore) Start() error {
 	}
 	//Create access to the will be routines of iot interfaces
 	f.ciCollection = make([]*ci, len(platforms))
-	f.ciCommonChannel = make(chan iotInterface.ComLinkMessage, 20)
+	f.ciCommonCH = make(chan iotInterface.ComLinkMessage, 20)
 	//Create the communication to the iot interface thread
 
 	//Startup already known interfaces
 	for index, pl := range platforms {
 		channel := make(chan iotInterface.ComLinkMessage, 5)
 		ctx, cancel := context.WithCancel(f.ctx)
-		f.ciCollection[index] = &ci{platform: pl, channel: channel, ctx: ctx, cancel: cancel}
+		f.ciCollection[index] = &ci{Platform: pl, Channel: channel, Ctx: ctx, Cancel: cancel}
 		f.startInterface(f.ciCollection[index])
 	}
 
 	for {
 		select {
-		case cm := <-f.controlChannel:
-			if err := f.executeCommand(&cm); err != nil {
+		case cm := <-f.controlCH:
+			if err := f.executeCommand(&cm.Message); err != nil {
 				log.Fatalf("Error in executeCommand! controlMessage: %v\n", cm)
+				cm.ResponseCH <- false
 			}
-		case clm := <-f.ciCommonChannel:
+			cm.ResponseCH <- true
+		case clm := <-f.ciCommonCH:
 			if err := f.handleCIMessage(&clm); err != nil {
 				log.Fatalf("Error in handleCIMessage! message: %v\n", clm)
 			}
@@ -149,7 +166,7 @@ func (f *Fogcore) listenOnTLS() error {
 					log.Print(x509.MarshalPKIXPublicKey(v.PublicKey))
 				}
 			}
-			go handleTLSClient(conn)
+			go f.handleTLSConn(conn)
 		case <-f.ctx.Done():
 			return nil
 		}
@@ -157,33 +174,270 @@ func (f *Fogcore) listenOnTLS() error {
 
 }
 
-func handleTLSClient(conn net.Conn) {
+func (f *Fogcore) handleTLSConn(conn net.Conn) {
+	buf := make([]byte, 2048)
+	for {
+		//Read
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Fatalf("fogcore: handleTLSConn: error: %v\n", err)
+		}
+		//var m tlsMessage
+		//err = json.Unmarshal(buf[0:n], m)
+		m, err := hecomm.NewMessage(buf[:n])
+		if err != nil {
+			log.Fatalf("fogcore: handleTLSConn: NewMessage: error: %v\n", err)
+		}
 
+		//Detect control message, is boolean 'Link' true or false?
+		switch m.FPort {
+		case 10:
+			bufProv := make([]byte, 2048)
+			ctx, cancel := context.WithTimeout(f.ctx, time.Minute*5)
+			defer cancel()
+			ls := linkState{
+				ReqConn: conn,
+				BufReq:  buf,
+				BufProv: bufProv,
+				Ctx:     ctx,
+			}
+			ls.handleLinkProtocol(m)
+
+		case 0:
+			//Unmarshal the data part of hecomm message as command
+			cm, err := m.GetCommand()
+			if err != nil {
+				log.Fatalf("fogcore: handleTLSConn: GetCommand error: %v\n", err)
+			}
+			resp := make(chan bool, 1)
+			cchm := controlCHMessage{
+				Message:    *cm,
+				ResponseCH: resp,
+			}
+			//Sending command to main routine, waiting for answer, also getting ready to close connection
+			f.controlCH <- cchm
+			defer conn.Close()
+			response := <-resp
+			rsp, err := (&hecomm.Response{OK: response}).GetBytes()
+			if err != nil {
+				log.Fatalf("fogcore: handleTLSConn: getbytes of response, error: %v\n", err)
+				return
+			}
+			//Writing answer to client
+			conn.Write(rsp)
+			//Stop connection
+			return
+		}
+
+	}
+}
+
+func (ls *linkState) handleLinkProtocol(sP *hecomm.Message) {
+	var message *hecomm.Message
+	var err error
+	var rcv []byte
+	rcvOrigFromReq := true
+	rcv = sP.Data
+	chReq := make(chan []byte, 10)
+	chProv := make(chan []byte, 10)
+	chError := make(chan error)
+
+	//Close channel when done
+	defer ls.ReqConn.Close()
+
+	//Tunnel data from requester to channel requester
+	go func(ch chan []byte, chError chan error, buf []byte) {
+		for {
+			n, err := ls.ReqConn.Read(buf)
+			if err != nil {
+				chError <- err
+				return
+			}
+			ch <- buf[:n]
+		}
+	}(chReq, chError, ls.BufReq)
+
+	for {
+
+		message, err = hecomm.NewMessage(rcv)
+		if err != nil {
+			log.Fatalf("fogcore: handleLinkProtocol: unable to unmarshal linkmessage: %v\n", err)
+		}
+
+		//Do action depending on type of message
+		switch message.FPort {
+
+		case hecomm.FPortLinkReq:
+			//TODO: check requesting node, in db?
+			lc, err := message.GetLinkContract()
+			if err != nil {
+				log.Fatalf("fogcore: handleLinkProtocol: unvalid link request packet: %v, error: %v\n", string(message.Data), err)
+				break
+			}
+			node, err := dbconnection.FindAvailableProviderNode(lc.InfType)
+			if err != nil {
+				log.Fatalf("fogcore: handleLinkProtocol")
+			}
+			if node.ID == 0 {
+				log.Fatalf("fogcore: handleLinkProtocol: Dit not find suitable provider node! link request: %v\n", string(sP.Data))
+				//Sending failed response
+				bytes, err := hecomm.NewResponse(false)
+				if err != nil {
+					log.Fatalf("fogcore: handleLinkProtocol: failed response, error: %v\n", err)
+				}
+				ls.ReqConn.Write(bytes)
+				return
+			}
+			platform, err := dbconnection.GetPlatform(node.ID)
+			if err != nil {
+				log.Fatalf("fogcore: handleLinkProtocol: getplatform of available node failed: %v\n", err)
+			}
+
+			//Setup tls connection to provider platform
+			conf := &tls.Config{
+			//InsecureSkipVerify: true,
+			}
+			ls.ProvConn, err = tls.Dial("tcp", platform.Address, conf)
+			if err != nil {
+				log.Fatalf("fogcore: handleLinkProtocol: could not reach provider platform: %v\n", err)
+				//TODO: connection not available
+				return
+			}
+			defer ls.ProvConn.Close()
+
+			//Send request for node to provider platform
+			/*bytes, err := json.Marshal(lMDataProvReq{
+				ProvNode: *node,
+			})*/
+			lc.ProvDevEUI = node.DevID
+			bytes, err := lc.GetBytes()
+			if err != nil {
+				log.Fatalf("fogcore: handleLinkProtocol: failed to send node request to provider platform, error: %v\n", err)
+				return
+			}
+			ls.ProvConn.Write(bytes)
+
+			//Sending OK response to requester
+			bytes, err = hecomm.NewResponse(true)
+			if err != nil {
+				log.Fatalf("fogcore: handleLinkProtocol: ok tslResponse, error: %v\n", err)
+				return
+			}
+			ls.ReqConn.Write(bytes)
+
+			//Tunnel data from requester to channel requester
+			go func(ch chan []byte, chError chan error, buf []byte) {
+				for {
+					n, err := ls.ReqConn.Read(buf)
+					if err != nil {
+						chError <- err
+						return
+					}
+					ch <- buf[:n]
+				}
+			}(chProv, chError, ls.BufProv)
+
+		case hecomm.FPortLinkState:
+			//Depending on origin of data send to the other
+			if rcvOrigFromReq {
+				ls.ProvConn.Write(message.Data)
+			} else {
+				ls.ReqConn.Write(message.Data)
+			}
+
+		case hecomm.FPortLinkSet:
+			lc, err := message.GetLinkContract()
+			if err != nil {
+				log.Fatalf("fogcore: handleLinkProtocol: unvalid data set packet: %v, error: %v\n", string(message.Data), err)
+				return
+			}
+			link, err := lc.ConvertToLink()
+			if err != nil {
+				log.Fatalf("fogcore: handleLinkProtocol: could not convert contract to link: contract: %v, error: %v\n", lc, err)
+			}
+			err = dbconnection.InsertLink(link)
+			if err != nil {
+				log.Fatalf("fogcore: handleLinkProtocol: could not insert link: contract: %v, error: %v\n", link, err)
+			}
+
+		default:
+			log.Fatalf("fogcore: handleLinkProtocol: unexpected FPort: %v\n", message.FPort)
+
+		}
+
+		//Wait for either packet from requester or data provider
+		select {
+		case rcv = <-chReq:
+			rcvOrigFromReq = true
+
+		case rcv = <-chProv:
+			rcvOrigFromReq = false
+
+		case err := <-chError:
+			log.Fatalf("fogcore: handleLinkProtocol: received error from a channel: %v\n", err)
+			return
+
+		case <-ls.Ctx.Done():
+			log.Fatalf("fogcore: handleLinkProtocol: context ended linkState: %v\n", ls)
+			return
+		}
+	}
 }
 
 //executeCommand Handle the control messages
-func (f *Fogcore) executeCommand(message *controlMessage) error {
-	switch message.command {
-	case "push": //Start new platform
-		channel := make(chan iotInterface.ComLinkMessage, 5)
-		ctx, cancel := context.WithCancel(f.ctx)
-		//Add new interface to end of collection
-		f.ciCollection = append(f.ciCollection, &ci{platform: message.platform, channel: channel, ctx: ctx, cancel: cancel})
-		//Startup last added interface
-		f.startInterface(f.ciCollection[len(f.ciCollection)-1])
+func (f *Fogcore) executeCommand(command *hecomm.DBCommand) error {
+	switch command.EType {
+	case hecomm.ETypePlatform: //Start new platform
+		//Unravel data from command packet into platform element
+		var platform dbconnection.Platform
+		err := json.Unmarshal(command.Data, platform)
+		if err != nil {
+			log.Fatalf("fogcore: executeCommand: unable to unmarshal platform from bytes: data: %v, err: %v\n", command.Data, err)
+		}
+		//Depending on insert bool, insert or delete
+		switch command.Insert {
+		case true:
+			channel := make(chan iotInterface.ComLinkMessage, 5)
+			ctx, cancel := context.WithCancel(f.ctx)
+			//Add new interface to end of collection
+			f.ciCollection = append(f.ciCollection, &ci{Platform: &platform, Channel: channel, Ctx: ctx, Cancel: cancel})
+			//Startup last added interface
+			f.startInterface(f.ciCollection[len(f.ciCollection)-1])
 
-	case "pull": //Stop a platform
-		for index, intface := range f.ciCollection {
-			if intface.platform.ID == message.platform.ID {
-				//Delete while preserving order
-				//append the slice part before the element with all the elements after the specific element
-				f.ciCollection = append(f.ciCollection[:index], f.ciCollection[index+1:]...)
+		case false: //Stop a platform
+			for index, intface := range f.ciCollection {
+				if intface.Platform.ID == platform.ID {
+					//Delete while preserving order
+					//append the slice part before the element with all the elements after the specific element
+					f.ciCollection = append(f.ciCollection[:index], f.ciCollection[index+1:]...)
+				}
+			}
+			//TODO: delete from db and certs?
+		}
 
+	case hecomm.ETypeNode:
+		var node dbconnection.Node
+		err := json.Unmarshal(command.Data, node)
+		if err != nil {
+			log.Fatalf("fogcore: executeCommand: unable to unmarshal node from bytes: data: %v, err: %v\n", command.Data, err)
+		}
+		//Depending on insert bool, insert or delete
+		switch command.Insert {
+		case true:
+			err := dbconnection.InsertNode(&node)
+			if err != nil {
+				log.Fatalf("fogcore: executeCommand: unable to insert node into db: node: %v, error: %v\n", node, err)
 			}
 
+		case false:
+			err := dbconnection.DeleteNode(node.ID)
+			if err != nil {
+				log.Fatalf("fogcore: executeCommand: unable to delete node from db: node: %v, error: %v\n", node, err)
+			}
 		}
+
 	default:
-		log.Fatalf("fogcore: unkown control message: %v\n", *message)
+		log.Fatalf("fogcore: executeCommand: unexpected EType: %v\n", *command)
 
 	}
 
@@ -195,15 +449,15 @@ func (f *Fogcore) startInterface(iot *ci) error {
 	//Starup the listening routines for all platforms in the database
 
 	//Depending on type, create iot interface routine
-	switch iot.platform.CIType {
+	switch iot.Platform.CIType {
 	case iotInterface.Lorawan:
 
 		//Convert general mapping to cilorawan Server Options
 		var args []grpc.ServerOption
-		cilorawan.ConvertArgsToUplinkOptions(iot.platform.CIArgs["uplink"], &args)
+		cilorawan.ConvertArgsToUplinkOptions(iot.Platform.CIArgs["uplink"], &args)
 
 		//args := (grpc.ServerOption)pl.CIArgs
-		lorawanapi := cilorawan.NewApplicationServerAPI(iot.ctx, iot.channel, args...)
+		lorawanapi := cilorawan.NewApplicationServerAPI(iot.Ctx, iot.Channel, args...)
 
 		//Start the cilorawan
 		go func() {
@@ -214,7 +468,7 @@ func (f *Fogcore) startInterface(iot *ci) error {
 		//Tunnel the communication to common channel -- easy access in main loop
 		go func() {
 			for {
-				f.ciCommonChannel <- <-iot.channel
+				f.ciCommonCH <- <-iot.Channel
 			}
 		}()
 
@@ -222,9 +476,9 @@ func (f *Fogcore) startInterface(iot *ci) error {
 		//Create the communication to the iot interface thread
 
 		var args cisixlowpan.ServerOptions
-		cisixlowpan.ConvertArgsToUplinkOptions(iot.platform.CIArgs["uplink"], &args)
+		cisixlowpan.ConvertArgsToUplinkOptions(iot.Platform.CIArgs["uplink"], &args)
 
-		sixlowpanServer := cisixlowpan.NewServer(iot.ctx, iot.channel, args)
+		sixlowpanServer := cisixlowpan.NewServer(iot.Ctx, iot.Channel, args)
 
 		//Start the cilorawan
 		go func() {
@@ -235,7 +489,7 @@ func (f *Fogcore) startInterface(iot *ci) error {
 		//Tunnel the communication to common channel -- easy access in main loop
 		go func() {
 			for {
-				f.ciCommonChannel <- <-iot.channel
+				f.ciCommonCH <- <-iot.Channel
 			}
 		}()
 
