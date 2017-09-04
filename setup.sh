d514d767133274c7872182b7730a5304d74d009a
joriwind <j.winderickx@gmail.com>
2017-09-04 15:31:00 +0200

    more script

--- setup.sh
+++ setup.sh
@@ -2,19 +2,32 @@
 PASS="winderickx"
 
 
+build(){
+    echo "Building executable..."
+    env GOOS=linux GOARCH=arm go build -o hecomm-fog
+}
+
+copy(){
+    echo "Copying files..."
+    sshpass -p "$PASS" scp hecomm-fog root@192.168.2.1:/tmp/
+    sshpass -p "$PASS" scp -r certs/ root@192.168.2.1:/tmp/
+}
+
 case $1 in
     "build")
-        echo "Building executable..."
-        env GOOS=linux GOARCH=arm go build -o hecomm-fog
+        build 
     ;;
     "router")
-        echo "Copying files..."
-        sshpass -p "$PASS" scp hecomm-fog root@192.168.2.1:/tmp/
-        sshpass -p "$PASS" scp -r certs/ root@192.168.2.1:/tmp/
+        copy
     ;;
+    "all")
+        build
+        copy
+        ;;
 
     *)
         echo "Unexpected command: $1; expected: build || router"
+    ;;
 
 
 esac
