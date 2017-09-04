c75dd9d8fad9d9788a754f288153df951fe59b96
joriwind <j.winderickx@gmail.com>
2017-09-04 15:04:36 +0200

    setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 
 PASS="winderickx"
 
-if [ "$1" = "router" ]
-then
 
-scp hecomm-fog root@192.168.2.1:/tmp/ -w "$PASS"
-scp -r certs/ root@192.168.2.1:/tmp/ -w "$PASS"
+case $1 in
+    "build")
+        echo "Building executable..."
+        env GOOS=linux GOARCH=arm go build -o hecomm-fog
+    ;;
+    "router")
+        echo "Copying files..."
+        sshpass -p "$PASS" scp hecomm-fog root@192.168.2.1:/tmp/
+        sshpass -p "$PASS" scp -r certs/ root@192.168.2.1:/tmp/
+    ;;
 
-fi
+    *)
+        echo "Unexpected command: $1; expected: build || router"
+
+
+esac
+echo "Done"
