72b43e98c25ac1b99e076d2951ec0b427d5cd397
joriwind <j.winderickx@gmail.com>
2017-09-07 16:36:01 +0200

    lora nwk server correct ip

--- iotInterface/cilorawan/config.go
+++ iotInterface/cilorawan/config.go
@@ -2,7 +2,7 @@ package cilorawan
 
 //Configuration of client and server
 const (
-	ConfNSAddress     string = "192.168.2.229:8000"
+	ConfNSAddress     string = "192.168.2.104:8000"
 	confCILorawanCert string = "certs/lorawan.pem"
 	confCILorawanKey  string = "certs/lorawan-key.pem"
 )
